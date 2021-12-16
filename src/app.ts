a3684366f75bf2f72efde5dc320c1dfe81d7255f
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-12-16 09:10:45 +0100

    update formatting rules

--- src/app.ts
+++ src/app.ts
@@ -17,32 +17,31 @@ const mongoUrl = MONGODB_URI
 
 mongoose.Promise = bluebird
 mongoose
-    .connect(mongoUrl)
-    .then(() => {
-        /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
-        console.log('Its finee')
-    })
-    .catch((err: Error) => {
-        console.log(
-            'MongoDB connection error. Please make sure MongoDB is running. ' +
-                err
-        )
-        process.exit(1)
-    })
+  .connect(mongoUrl)
+  .then(() => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+    console.log('MongoDB connected')
+  })
+  .catch((err: Error) => {
+    console.log(
+      'MongoDB connection error. Please make sure MongoDB is running. ' + err
+    )
+    process.exit(1)
+  })
 
 // Express configuration
 app.set('port', process.env.PORT || 3000)
 
 // Configure SASS
 app.use(
-    sassMiddleware({
-        /* Options */
-        src: __dirname,
-        dest: path.join(__dirname, 'public/css'),
-        debug: true,
-        outputStyle: 'compressed',
-        prefix: '/prefix' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
-    })
+  sassMiddleware({
+    /* Options */
+    src: __dirname,
+    dest: path.join(__dirname, 'public/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/prefix' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+  })
 )
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
@@ -63,7 +62,7 @@ app.use(lusca.xssProtection(true))
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.render('home')
+  res.render('home')
 })
 
 export default app
